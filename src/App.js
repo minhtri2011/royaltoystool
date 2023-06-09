1bf2697fe30150266ca8707762b15e0b15a1ee9b
minhtri2011 <minhtringuyenemailcomtri3@gmail.com>
2023-06-09 17:49:14 +0700

    update favion

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
 import { Toaster } from "react-hot-toast";
+import TagsPage from "./pages/tagsPage";
 
 const routers = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/tags",
+    element: <TagsPage />,
   },
 ]);
 function App() {
---
+++ src/pages/tagsPage.jsx
@@ -0,0 +1,18 @@
+import { Button } from '@chakra-ui/react'
+import React from 'react'
+
+const TagsPage = () => {
+  return (
+    <div>
+        <Button>Kit</Button>
+        <Button>Metabuild</Button>
+        <Button>Resin</Button>
+        <Button>Moshow MB</Button>
+        <Button>Motor Nuclear MB</Button>
+        <Button>Motor Nuclear kit</Button>
+        <Button>Kit</Button>
+    </div>
+  )
+}
+
+export default TagsPage
