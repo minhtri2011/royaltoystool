a7a83ae0f647d83b1cacbad2e32f9c215daf3710
minhtri2011 <minhtringuyenemailcomtri3@gmail.com>
2023-09-03 20:11:32 +0700

    update giveaway

--- src/pages/giveaway.jsx
+++ src/pages/giveaway.jsx
@@ -14,7 +14,7 @@ const Giveaway = () => {
     const template = `𝐄𝐕𝐄𝐍𝐓 𝐆𝐈𝐕𝐄 𝐀𝐖𝐀𝐘  😝
 
 Lưu ý trước khi chơi: 
-_Vui lòng đọc hết để làm đủ các bước và nắm bắt được tình hình :3
+_Vui lòng đọc kĩ để làm đủ các bước và nắm bắt được tình hình 
 _Không quan tâm ae có mua hàng hay không, cứ tham gia hết mình vì một cộng đồng lành mạnh
 
 𝑷𝒉𝒂̂̀𝒏 𝒕𝒉𝒖̛𝒐̛̉𝒏𝒈: ${copyValue.gift}
@@ -22,21 +22,24 @@ _Không quan tâm ae có mua hàng hay không, cứ tham gia hết mình vì m�
 -------------------------------------------------
 Cách thức tham gia:
 
-1️⃣ 𝗕𝘂̛𝗼̛́𝗰 𝟭: Like + Follow fanpage 𝐑𝐨𝐲𝐚𝐥 𝐓𝐨𝐲𝐬 và 𝐑𝐨𝐲𝐚𝐥 𝐓𝐨𝐲𝐬 - 𝐅𝐢𝐠𝐮𝐫𝐞 để không bỏ lỡ bất kỳ thông tin nào về sự kiện và cập nhật các mô hình mới nhất:
+𝗕𝘂̛𝗼̛́𝗰 𝟭: Like + Follow fanpage 𝐑𝐨𝐲𝐚𝐥 𝐓𝐨𝐲𝐬 và 𝐑𝐨𝐲𝐚𝐥 𝐓𝐨𝐲𝐬 - 𝐅𝐢𝐠𝐮𝐫𝐞 để không bỏ lỡ bất kỳ thông tin nào về sự kiện và cập nhật các mô hình mới nhất:
 _𝐑𝐨𝐲𝐚𝐥 𝐓𝐨𝐲𝐬 - 𝐅𝐢𝐠𝐮𝐫𝐞: https://www.facebook.com/RoyaltoysCollectibles999
 _𝐑𝐨𝐲𝐚𝐥 𝐓𝐨𝐲𝐬: https://www.facebook.com/royaltoys9999
 
-2️⃣ 𝗕𝘂̛𝗼̛́𝗰 𝟮: Like và Chia sẻ bài viết này lên trang cá nhân ở chế độ công khai kèm nội dung: 
-${copyValue.content}
+𝗕𝘂̛𝗼̛́𝗰 𝟮: Like và Chia sẻ bài viết này lên trang cá nhân ở chế độ công khai ${copyValue.content ? 'kèm nội dung ' +copyValue.content: ''}
 
-3️⃣ 𝗕𝘂̛𝗼̛́𝗰 𝟯: Comment dưới bài viết này với 3 số cuối giải đặc biệt của sổ số miền Bắc ngày ${copyValue.date} mà bạn dự đoán kèm tag ít nhất 3 người bạn để cùng tham gia
+𝗕𝘂̛𝗼̛́𝗰 𝟯: Tham gia vào hai nhóm
+_𝐂𝐨̣̂𝐧𝐠 đ𝐨̂̀𝐧𝐠 𝐀𝐧𝐭𝐢 𝐒𝐜𝐚𝐦 𝐆𝐮𝐧𝐝𝐚𝐦 𝐕𝐍: https://www.facebook.com/groups/311652321168639
+_𝐂𝐡𝐨̛̣ 𝐭𝐫𝐨̛̀𝐢 𝐆𝐮𝐧𝐝𝐚𝐦 𝐕𝐍: https://www.facebook.com/groups/606837867234057
+
+𝐁𝐮̛𝐨̛́𝐜 𝟒 Comment dưới bài viết này với 3 số cuối giải đặc biệt của sổ số miền Bắc ngày ${copyValue.date} mà bạn dự đoán kèm tag ít nhất 3 người bạn để cùng tham gia
 -------------------------------------------------
 
 𝑯𝒂̣𝒏 𝒆𝒗𝒆𝒏𝒕: đến 5h ngày ${copyValue.date} 
 
 Tui sẽ công bố kết quả vào tối ${copyValue.date} và không chấp nhận comment đã chỉnh sửa hoặc lố giờ
 
-Đợt này chúng ta sẽ delay thêm hai ngày cuối để anh em kháng cáo kết quả, nếu qua hai ngày này không ai có ý kiến tui sẽ trao phần thưởng cho người có kết quả chính xác nhất, trường hợp không ai trúng tui sẽ chọn con số gần nhất với kết quả và comment sớm nhất nếu có hai người trùng nhé!!!
+Đợt này chúng ta sẽ delay thêm hai ngày cuối để anh em kháng cáo kết quả, nếu qua hai ngày này không ai có ý kiến tui sẽ trao phần thưởng cho người có kết quả chính xác và sớm nhất nhé!!!
 ${tags}${resinTag} ${kitTag} ${metalBuildTag} #giveAway
 `;
 
